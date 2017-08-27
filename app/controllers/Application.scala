228ef8496290180d002dd67c21a57cb9c0188067
Monique Moledo <moniquemoledo@hotmail.com>
2017-08-27 01:33:08 -0300

    Changes request to POST

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,24 +2,23 @@ package controllers
 
 import com.google.inject.Inject
 import integration.DandelionIntegration
-import io.swagger.annotations.{Api, ApiParam, ApiResponse, ApiResponses}
+import io.swagger.annotations.{Api, ApiResponse, ApiResponses}
 import play.api.mvc._
+
 import scala.concurrent.ExecutionContext.Implicits.global
 
 @Api
  class Application @Inject() (dandelion: DandelionIntegration) extends Controller {
 
-   def index = Action {
-     Ok(views.html.index())
-   }
+  @ApiResponses(Array(
+    new ApiResponse(code = 400, message = "Invalid text supplied"),
+    new ApiResponse(code = 404, message = "Location not found")))
+   def getLocationsByText() = Action.async {
 
-   @ApiResponses(Array(
-     new ApiResponse(code = 400, message = "Invalid text supplied"),
-     new ApiResponse(code = 404, message = "Location not found")))
-   def getLocationsByText(@ApiParam(value = "Text which locations should be extracted") text: String) = Action.async {
-
+     request =>
+       val text = request.body.asJson.get.\("text").get.toString()
      val futureResult = dandelion.extractEntities(text)
 
-     futureResult.map(i => Ok("Got result: " + dandelion.jsonParser(i.body).toString()))
+     futureResult.map(extractedLocation => Ok("Got result: " + dandelion.jsonParser(extractedLocation.body).toString()))
    }
 }
