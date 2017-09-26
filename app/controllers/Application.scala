585e8ef811ef97baf798b8fcd21fa0d66787a4e1
Monique Moledo <moniquemoledo@hotmail.com>
2017-09-26 13:21:31 -0300

    :fire: Removes swagger

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,18 +2,13 @@ package controllers
 
 import com.google.inject.Inject
 import integration.DandelionIntegration
-import io.swagger.annotations.{Api, ApiResponse, ApiResponses}
 import map.Mapper
 import play.api.mvc._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-@Api
  class Application @Inject() (mapper: Mapper, dandelion: DandelionIntegration) extends Controller {
 
-  @ApiResponses(Array(
-    new ApiResponse(code = 400, message = "Invalid text supplied"),
-    new ApiResponse(code = 500, message = "Internal Server Error")))
    def getLocationsByText() = Action.async(parse.json(maxLength = 1024 * 500)) {
 
      request =>
--- build.sbt
+++ build.sbt
@@ -8,10 +8,6 @@ scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq( jdbc , cache , ws , specs2 % Test , guice)
 
-libraryDependencies ++= Seq(
-  "io.swagger" %% "swagger-play2" % "1.6.1-SNAPSHOT"
-)
-
 unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
 
 resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"
